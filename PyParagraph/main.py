0f7dc679a76a90d5a140fdf01384742b4d156e27
Schrodinger <35243347+cburdon@users.noreply.github.com>
2018-05-05 16:31:46 -0500

    Final edit of PyParagraph HW to fix issues with multi-line txt files.

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -3,21 +3,24 @@ import os
 
 'ask for user to input text file, then add .txt extension and open that file'
 userfile = input("Which file would you like to open?   ")
-sentenceCount = 0
 words = 0
-with open(userfile + ".txt", "r") as text:
-    'iterate through each line of the file, construct wordArray out of each separate word'
-    'get count of words from length of array'   
-    for line in text:
-        wordArray = line.split(" ") 
-        wordCount = len(wordArray)
-        'add up total number of words using wordCount range, then divide to calculate average letters'
-        for i in range(0, wordCount):
-            words += len(wordArray[i])
-        lettercount = words/wordCount
-        'split by punctuation to get sentence count, then divide by wordCount for sentence length'
-        sentenceCount += len(re.split("(?<=[.!?]) +" , line))
-        sentenceLength = wordCount/sentenceCount
+wordArray = []
+paragraph = open(userfile + ".txt").read()
+'iterate through each line of the file, construct wordArray out of each separate word'
+'get count of words from length of array'   
+wordArray = (paragraph.split(" ")) 
+wordCount = len(wordArray)
+'add up total number of words using wordCount range, then divide to calculate average letters'
+for i in range(0, wordCount):
+    words += len(wordArray[i])
+lettercount = words/wordCount
+'split by punctuation to get sentence count, then divide by wordCount for sentence length'
+sentencetemp = re.split("(?<=[.!?]) +" , paragraph)
+'''regex split does not work for multi-line files, splitting by periods works in this case 
+because all input files use no other punctuation.'''
+sentencetemp2 = paragraph.split(".")
+sentenceCount = len(sentencetemp2) - 1
+sentenceLength = wordCount/sentenceCount
 'create a new txt file in Output folder and print results'
 output = os.path.join("Output", userfile + ".txt")
 
